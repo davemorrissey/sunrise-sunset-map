f79f177c520da711c81650a4af17dc75d35203c8
David Morrissey <davemorrissey@gmail.com>
2017-11-13 13:39:21 +0000

    Further refactoring to nested structure

--- app/src/data/DateSelector.js
+++ app/src/data/DateSelector.js
@@ -62,72 +62,69 @@ class DateSelector extends Component {
   }
   
   render() {
-    let buttons = <div/>;
+    let nav = null;
     if (this.state.type === 'buttons') {
-      buttons = <div>
-        <button className="back" onClick={this.dateDiff.bind(this, '-m')}><i className="fa fa-caret-left"/> M</button>
-        <button className="back" onClick={this.dateDiff.bind(this, '-w')}><i className="fa fa-caret-left"/> W</button>
-        <button className="back" onClick={this.dateDiff.bind(this, '-d')}><i className="fa fa-caret-left"/> D</button>
-        <button className="forward" onClick={this.dateDiff.bind(this, '+d')}>D <i className="fa fa-caret-right"/></button>
-        <button className="forward" onClick={this.dateDiff.bind(this, '+w')}>W <i className="fa fa-caret-right"/></button>
-        <button className="forward" onClick={this.dateDiff.bind(this, '+m')}>M <i className="fa fa-caret-right"/></button>
-      </div>
-    }
-    var thisDate = new Date(this.state.calendar.getFullYear(), this.state.calendar.getMonth(), 1);
 
-    let rows = [];
-    for (var r = 0; r < 6; r++) {
-      let row = [];
-      for (var c = 0; c < 7; c++) {
-        if (r === 0 && c < thisDate.getDay()) {
-          // Leading blanks
-          row.push(<td key={''+r+c}>&nbsp;</td>);
-        } else if (thisDate.getMonth() === this.state.calendar.getMonth()) {
-          // Date cells
-          var cellDate = new Date(thisDate.getFullYear(), thisDate.getMonth(), thisDate.getDate());
-          var className = this.dateDmy(this.props.date) === this.dateDmy(cellDate) ? 'selected' : '';
-          row.push(<td key={''+r+c} className="date"><a onClick={this.props.setDate.bind(this, cellDate)} className={className}>{cellDate.getDate()}</a></td>);
-          thisDate.setDate(thisDate.getDate() + 1);
-        } else {
-          // Trailing blanks
-          row.push(<td key={''+r+c}><a>&nbsp;</a></td>);
+      nav = <div className="buttons">
+        <button onClick={this.dateDiff.bind(this, '-m')}><i className="fa fa-caret-left"/> M</button>
+        <button onClick={this.dateDiff.bind(this, '-w')}><i className="fa fa-caret-left"/> W</button>
+        <button onClick={this.dateDiff.bind(this, '-d')}><i className="fa fa-caret-left"/> D</button>
+        <button onClick={this.dateDiff.bind(this, '+d')}>D <i className="fa fa-caret-right"/></button>
+        <button onClick={this.dateDiff.bind(this, '+w')}>W <i className="fa fa-caret-right"/></button>
+        <button onClick={this.dateDiff.bind(this, '+m')}>M <i className="fa fa-caret-right"/></button>
+      </div>
+
+    } else {
+
+      var thisDate = new Date(this.state.calendar.getFullYear(), this.state.calendar.getMonth(), 1);
+
+      let rows = [];
+      for (var r = 0; r < 6; r++) {
+        let row = [];
+        for (var c = 0; c < 7; c++) {
+          if (r === 0 && c < thisDate.getDay()) {
+            // Leading blanks
+            row.push(<td key={''+r+c}>&nbsp;</td>);
+          } else if (thisDate.getMonth() === this.state.calendar.getMonth()) {
+            // Date cells
+            var cellDate = new Date(thisDate.getFullYear(), thisDate.getMonth(), thisDate.getDate());
+            var className = this.dateDmy(this.props.date) === this.dateDmy(cellDate) ? 'selected' : '';
+            row.push(<td key={''+r+c} className="date"><a onClick={this.props.setDate.bind(this, cellDate)} className={className}>{cellDate.getDate()}</a></td>);
+            thisDate.setDate(thisDate.getDate() + 1);
+          } else {
+            // Trailing blanks
+            row.push(<td key={''+r+c}><a>&nbsp;</a></td>);
+          }
         }
+        rows.push(<tr key={r}>{row}</tr>);
       }
-      rows.push(<tr key={r}>{row}</tr>);
-    }
 
-    let table = <table className="datepicker">
-        <thead>
-          <tr>
-            <td><a onClick={this.calendarMove.bind(this, -12)}><i className="fa fa-angle-double-left"/></a></td>
-            <td><a onClick={this.calendarMove.bind(this, -1)}><i className="fa fa-angle-left"/></a></td>
-            <td className="title" colSpan="3">{this.monthArrayShort[this.state.calendar.getMonth()] + ' ' + this.state.calendar.getFullYear()}</td>
-            <td><a onClick={this.calendarMove.bind(this, 1)}><i className="fa fa-angle-right"/></a></td>
-            <td><a onClick={this.calendarMove.bind(this, 12)}><i className="fa fa-angle-double-right"/></a></td>
-          </tr>
-          <tr>
-            { this.dayArrayShort.map(dayShort => <th key={dayShort}>{dayShort}</th>) }
-          </tr>
-        </thead>
-        <tbody>
-          { rows }
-        </tbody>
-      </table>;
+      nav = <table className="picker">
+          <thead>
+            <tr>
+              <td><a onClick={this.calendarMove.bind(this, -12)}><i className="fa fa-angle-double-left"/></a></td>
+              <td><a onClick={this.calendarMove.bind(this, -1)}><i className="fa fa-angle-left"/></a></td>
+              <td className="title" colSpan="3">{this.monthArrayShort[this.state.calendar.getMonth()] + ' ' + this.state.calendar.getFullYear()}</td>
+              <td><a onClick={this.calendarMove.bind(this, 1)}><i className="fa fa-angle-right"/></a></td>
+              <td><a onClick={this.calendarMove.bind(this, 12)}><i className="fa fa-angle-double-right"/></a></td>
+            </tr>
+            <tr>
+              { this.dayArrayShort.map(dayShort => <th key={dayShort}>{dayShort}</th>) }
+            </tr>
+          </thead>
+          <tbody>
+            { rows }
+          </tbody>
+        </table>;
 
-    let calendar = <div/>;
-    if (this.state.type === 'calendar') {
-      calendar = <div id="datepicker-wrapper">
-        <div id="datepicker">{table}</div>
-      </div>
     }
 
     let dateLong = this.dayArrayMed[this.props.date.getDay()] + ' ' + this.props.date.getDate() + ' ' + this.monthArrayLong[this.props.date.getMonth()] + ' ' + this.props.date.getFullYear();
 
     return (
-      <div id="datenav">
-        <div id="date-current">{dateLong}<a id="date-picker-toggle" onClick={this.toggleDatePicker.bind(this)}><i className="fa fa-calendar"/></a></div>
-        {buttons}
-        {calendar}
+      <div id="dateselector">
+        <div className="current">{dateLong}<a className="toggle" onClick={this.toggleDatePicker.bind(this)}><i className="fa fa-calendar"/></a></div>
+        {nav}
       </div>
     );
   }
