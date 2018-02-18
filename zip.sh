9635e5273d6ce902de8fa0f48b27fb37ab815872
David Morrissey <davemorrissey@gmail.com>
2018-02-18 08:18:18 +0000

    Zip script style fixes and shebang

--- zip.sh
+++ zip.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 DIR=sunrise-sunset-map
 composer install
 cd app
 npm run build
 cd ..
-rm -rf $DIR
+rm -rf ${DIR}
 rm sunrise-sunset-map.zip
-mkdir $DIR
-mkdir $DIR/app
-cp -r vendor $DIR
-cp -r api $DIR
-cp -r app/build $DIR/app
-zip -r sunrise-sunset-map.zip $DIR
-rm -rf $DIR
+mkdir ${DIR}
+mkdir ${DIR}/app
+cp -r vendor ${DIR}
+cp -r api ${DIR}
+cp -r app/build ${DIR}/app
+zip -r sunrise-sunset-map.zip ${DIR}
+rm -rf ${DIR}
